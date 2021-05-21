1a8befd171c16d87c60e0846f2bb01bd21da6ce2
Elvin0078 <canbaxisovekvin@gmail.com>
2021-05-21 02:51:20 -0700

    Get Jwt Token

--- src/main/java/com/example/spring_jwt_token_test/security/SecurityConfigurer.java
+++ src/main/java/com/example/spring_jwt_token_test/security/SecurityConfigurer.java
@@ -1,16 +1,12 @@
 package com.example.spring_jwt_token_test.security;
 
-import org.apache.catalina.filters.RequestFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
